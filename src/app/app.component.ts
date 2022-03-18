5596de4973e54c3257ec1bb0e4f330ec49f8ae3f
Huzefa Battiwala <huzaifa.hb.123@gmail.com>
2022-03-18 18:39:05 +0500

    components and life-cycle hooks

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,9 +2,23 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  // template: `<p>Hello World</p>`,
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css'],
-  // styles: [''],
 })
-export class AppComponent {}
+export class AppComponent {
+  name = 'Huzefa';
+  imageURL =
+    'https://i.picsum.photos/id/237/500/500.jpg?hmac=idOEkrJhLd7nEU5pNrAGCyJ6HHJdR_sit1qDt5J3Wo0';
+
+  getName() {
+    return this.name;
+  }
+
+  changeImage(e: KeyboardEvent) {
+    this.imageURL = (e.target as HTMLInputElement).value;
+  }
+
+  logImg(event: string) {
+    console.log(event);
+  }
+}
