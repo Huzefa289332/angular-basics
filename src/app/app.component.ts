e997c2e89226c5cba08cb80c721c17c0ab78f242
Huzefa Battiwala <huzaifa.hb.123@gmail.com>
2022-03-18 21:13:17 +0500

    pipes and directives

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,9 +6,23 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  name = 'Huzefa';
+  name = 'huzefa mustafa';
   imageURL =
     'https://i.picsum.photos/id/237/500/500.jpg?hmac=idOEkrJhLd7nEU5pNrAGCyJ6HHJdR_sit1qDt5J3Wo0';
+  images = [
+    'https://i.picsum.photos/id/237/500/500.jpg?hmac=idOEkrJhLd7nEU5pNrAGCyJ6HHJdR_sit1qDt5J3Wo0',
+    'https://i.picsum.photos/id/289/200/300.jpg?hmac=TVh4H_Hra3e1VSDPJz-mhCgep32qIa7T6DGQvbrjMb4',
+    'https://i.picsum.photos/id/237/500/500.jpg?hmac=idOEkrJhLd7nEU5pNrAGCyJ6HHJdR_sit1qDt5J3Wo0',
+  ];
+  currentDate = new Date();
+  cost = 2000;
+  temperature = 25.3;
+  pizza = {
+    toppings: ['pepperoni', 'bacon'],
+    size: 'large',
+  };
+  blueClass = false;
+  fontSize = 16;
 
   getName() {
     return this.name;
